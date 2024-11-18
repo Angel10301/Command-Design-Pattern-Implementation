f8a13474a1676fd19e9d30b018d2ab23039d72e4
Can SEFEROGLU <can.seferoglu@gmail.com>
2024-11-18 03:53:26 -0800

    Update CDpattern.cpp
    Add a new interface "Interactible". 
    Make Door implement Interactible and add DoorBell, which also does so.
    Add knock and ring features.
    Clean some spaces.

--- CDpattern.cpp
+++ CDpattern.cpp
@@ -6,10 +6,24 @@ public:
     virtual ~Command() = default; //destructor to cleanup derived classes
 };
 
-class Door { //Receiver
+class Interactible{
 public:
-    void open(){std::cout<< "Door is open." << std::endl;}
-    void close(){ std:: cout << "Door is closed." << std::endl;}
+    virtual void signalPresence() = 0;
+};
+
+class DoorBell : public Interactible{ //Receiver
+    Interactible* inter;
+public:
+    void signalPresence() override {std::cout << "Doorbell rang." << std::endl;}
+};
+
+class Door : public Interactible{ //Receiver
+    Interactible* inter;
+public:
+    void signalPresence() override {std::cout << "Door knocked." << std::endl;}
+    void knock(){std::cout << "Door knocked." << std::endl;}
+    void open(){std::cout<< "Door opened." << std::endl;}
+    void close(){std::cout << "Door closed." << std::endl;}
 };
 
 class OpenDoor : public Command { //concrete Commands
@@ -26,6 +40,20 @@ public:
     void execute() override { door->close();} //close door
 };
 
+class KnockDoor : public Command { 
+    Door* door;
+public:
+    KnockDoor(Door* d) : door(d) {}
+    void execute() override { door->signalPresence();} //knock door
+};
+
+class RingDoor : public Command { 
+    DoorBell* doorbell;
+public:
+    RingDoor(DoorBell* d) : doorbell(d) {}
+    void execute() override { doorbell->signalPresence();} //knock door
+};
+
 class DoorControl{ //invoker
     Command* command;
 public:
@@ -35,11 +63,20 @@ public:
 
 int main(){
     Door door; //create Door object aka receiver
+    DoorBell doorbell; //create Door object aka receiver
     OpenDoor openCommand(&door); //command to open door, assoicated with Door object
     CloseDoor closeCommand(&door); //command to close, associated with Door object
+    KnockDoor knockCommand(&door); //command to knock, associated with Door object
+    RingDoor ringCommand(&doorbell); //command to knock, associated with DoorBell object
 
     DoorControl control; //invoker that executes the commands
 
+    control.setCommand(&knockCommand);
+    control.pressButton();
+    
+    control.setCommand(&ringCommand);
+    control.pressButton();
+    
     control.setCommand(&openCommand); //sets command to "OpenDoor"
     control.pressButton(); //"open" command, outputs: "Door is open"
 
