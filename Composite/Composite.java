126ed5435e8e69f027475fd064c07aee3e7e3691
Can SEFEROGLU <can.seferoglu@gmail.com>
2024-12-03 23:01:45 -0800

    Fix Composite.java and move to Composite/

--- Composite/Composite.java
+++ Composite/Composite.java
@@ -1,8 +1,30 @@
 //The composite design pattern uses composition to represent a tree structure
-// It consists of TreeComponent a basic class, Leaf that represents individual leaves in the structure, and Branch that are branches that can contain other branches or leaves.
+//It consists of TreeComponent a basic class, Leaf that represents individual leaves in the structure, and Branch that are branches that can contain other branches or leaves.
 
 import java.util.ArrayList;
 import java.util.List;
+
+public class Main{
+    public static void main(String[] args) { // Creating individual leaf nodes
+
+        TreeComponent leaf1 = new Leaf("Leaf A"); // Create leaves
+        TreeComponent leaf2 = new Leaf("Leaf B");
+        TreeComponent leaf3 = new Leaf("Leaf C");
+
+        Branch branch1 = new Branch("Branch 1"); // Create branch and add the children
+        branch1.addChild(leaf1);
+        branch1.addChild(leaf2);
+
+        Branch branch2 = new Branch("Branch 2"); // Again Creating branch and adding the children
+        branch2.addChild(leaf3);
+
+        Branch mainTree = new Branch("Main Tree"); // Create main tree (root branch)
+        mainTree.addChild(branch1);
+        mainTree.addChild(branch2);
+
+        mainTree.operation(); // Display the tree structure
+    }
+}
 
 interface TreeComponent {// Abstract component
     void operation(); // Method to be implemented by all components
@@ -40,26 +62,3 @@ class Branch implements TreeComponent { // Branch class representing composite e
         }
     }
 }
-
-public class Main{
-    public static void main(String[] args) { // Creating individual leaf nodes
-
-        TreeComponent leaf1 = new Leaf("Leaf A"); // Create leaves
-        TreeComponent leaf2 = new Leaf("Leaf B");
-        TreeComponent leaf3 = new Leaf("Leaf C");
-
-        Branch branch1 = new Branch("Branch 1"); // Create branch and add the children
-        branch1.addChild(leaf1);
-        branch1.addChild(leaf2);
-
-        Branch branch2 = new Branch("Branch 2"); // Again Creating branch and adding the children
-        branch2.addChild(leaf3);
-
-        Branch mainTree = new Branch("Main Tree"); // Create main tree (root branch)
-        mainTree.addChild(branch1);
-        mainTree.addChild(branch2);
-
-        mainTree.operation(); // Display the tree structure
-    }
-}
-
